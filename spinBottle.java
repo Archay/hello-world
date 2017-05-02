debc1146ac3a2c364ad19d9f92aba640e570cf40
Archay <archay.dutta@gmail.com>
2017-05-02 19:02:01 +0400

    BottleSpinner
    Still has duplicacy on spins

--- spinBottle.java
+++ spinBottle.java
@@ -1,24 +1,34 @@
 
 Spinning A Bottle
 
+import java.util.Scanner;
+import java.util.Random;
 public class NewMain {
-    import java.util.Scanner;
-    import java.util.Random;
+
+    
     public static void main(String[] args) {
        
        int j;
+       int re = 999;
        Scanner userIn = new Scanner(System.in);
+       System.out.println("Number of players?");
        j = userIn.nextInt();
        String [] pplPool = new String [j];
-       for(int i = 1; i <= j; i = i+1){
+       for(int i = 1; i <= j; ){
+           System.out.println("Player" + i + ":");
            pplPool[i-1] = userIn.next();
+           i++;
+       }
+    while (re != 1337) {
+        Random spin = new Random();
+        int draw1 = spin.nextInt(j);
+        int draw2 = spin.nextInt(j);
+        System.out.println("(("+pplPool[draw1] + " asks " + pplPool[draw2]+"))");
+        System.out.println("1337 to stop");
+        re = userIn.nextInt();
         }
-       Random spin = new Random();
-       int quest = spin.nextInt(j+1);
-       System.out.println(pplPool[quest]);
-    
        
-        }
     }
-  
     
+}
+
